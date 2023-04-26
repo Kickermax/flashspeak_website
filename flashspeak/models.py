6739f309d70e3ec545e572a7e9b37bcb005ef671
Kim Maxim <kim.imax0721@yandex.ru>
2023-04-26 02:02:15 +0200

    create models for anki cards

---
+++ flashspeak/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Language(models.Model):
+    """
+    Model representing a single language.
+    """
+    language_id = models.AutoField(primary_key=True)
+    language_name = models.CharField(max_length=255)
+    language_image = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        """
+        String for representing the language name.
+        """
+        return self.language_name
+
+
+class Cards(models.Model):
+    """
+    Model representing a deck of cards (not a single card)
+    """
+    cards_id = models.AutoField(primary_key=True)
+    cards_name = models.CharField(max_length=255)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=255, null=True, blank=True)
+    status = models.PositiveSmallIntegerField(default=0)
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        String for representing the deck name.
+        """
+        return self.cards_name
+
+    def get_absolute_url(self):
+        """
+        Generate the URL for the Deck of Cards detail view.
+
+        Returns:
+            str: Absolute URL of the Deck of Cards instance.
+        """
+        return reverse('view_name', kwargs={'cards_id': self.cards_id})
+        #todo шаблон написать потом
+
+
+class Card(models.Model):
+    """
+    Model representing a single card
+    """
+    cards = models.ForeignKey(Cards, on_delete=models.CASCADE)
+    card_id = models.AutoField(primary_key=True)
+    front_card = models.CharField(max_length=255)
+    back_card = models.CharField(max_length=255)
+    image_card = models.CharField(max_length=255, null=True, blank=True)
+    REPEAT_CHOICES = [
+        (0, 'Again (< 1 day)'),
+        (1, 'One day later'),
+        (2, 'Four days later'),
+        (3, 'Twenty-one days later')
+    ]
+    repeat = models.PositiveSmallIntegerField(choices=REPEAT_CHOICES, default=0)
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        String for representing the card content.
+        """
+        return f"{self.front_card} - {self.back_card}"
+
+    def get_absolute_url(self):
+        """
+        Generate the URL for the Card detail view.
+
+        Returns:
+            str: Absolute URL of the Card instance.
+        """
+        return reverse('view_name', kwargs={'card_id': self.card_id})
+        #todo шаблон написать потом
+
--- flashspeak/settings.py
+++ flashspeak/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-import secret_settings
+from .secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flashspeak'
 ]
 
 MIDDLEWARE = [
